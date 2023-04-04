d932f778e8658608d6999270fe22c7d7c9c63e4c
kelseyjoshuaphillips <phil0382@algonquinlive.com>
2023-04-04 14:05:52 -0400

    Add initial project files

---
+++ Models/Student.cs
@@ -0,0 +1,25 @@
+﻿using Swashbuckle.AspNetCore.Annotations;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace Lab6.Models
+{
+    public class Student
+    {
+        [SwaggerSchema(ReadOnly = true)]
+        public Guid Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "First Name")]
+        public String FirstName { get; set; }
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "Last Name")]
+        public String LastName { get; set; }
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "Program")]
+        public String Program { get; set; }
+    }
+}
